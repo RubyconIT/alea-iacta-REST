0b7ae3e702e6a5d01b0a34a3e70743290d35b356
Elia <e.gamberi@advenias.it>
2025-08-26 11:39:26 +0200

    :sparkles: #2 new function: roman to integer

--- tools/integer.rb
+++ tools/integer.rb
@@ -1,19 +1,20 @@
-ROMAN_CONV = {
-  1000 => "M",
-  900 => "CM",
-  500 => "D",
-  400 => "CD",
-  100 => "C",
-  90 => "XC",
-  50 => "L",
-  40 => "XL",
-  10 => "X",
-  9 => "IX",
-  5 => "V",
-  4 => "IV",
-  1 => "I"
-}
 class Integer
+
+  ROMAN_CONV = {
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I"
+  }.freeze
 
   # SOURCE: https://stackoverflow.com/a/26103503
   def to_roman
